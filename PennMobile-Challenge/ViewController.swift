3026048a5330881bb4bd17777d2dfa9e938b425d
brian bae <brianbae@nichols-ve501-0077.apn.wlan.upenn.edu>
2018-09-22 14:51:50 -0400

    Initial Commit

---
+++ PennMobile-Challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PennMobile-Challenge
+//
+//  Created by brian bae on 2018. 9. 22..
+//  Copyright © 2018년 brian bae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PennMobile-ChallengeTests/PennMobile_ChallengeTests.swift
@@ -0,0 +1,36 @@
+//
+//  PennMobile_ChallengeTests.swift
+//  PennMobile-ChallengeTests
+//
+//  Created by brian bae on 2018. 9. 22..
+//  Copyright © 2018년 brian bae. All rights reserved.
+//
+
+import XCTest
+@testable import PennMobile_Challenge
+
+class PennMobile_ChallengeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
